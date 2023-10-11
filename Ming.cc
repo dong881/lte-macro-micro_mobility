294731958614169939e92ef5881555fce050d755
dong881 <joe888joejoe@gmail.com>
2023-10-12 04:04:58 +0800

    /ns3.40-Ming-default died with<Signals.SIGABRT: 6>

--- Ming.cc
+++ Ming.cc
@@ -1,25 +1,22 @@
 #include <ns3/core-module.h>
+
 #include <ns3/network-module.h>
 #include <ns3/mobility-module.h>
 #include <ns3/lte-module.h>
 #include "ns3/netanim-module.h"
 #include "ns3/flow-monitor-module.h"
-#include "ns3/internet-module.h" // Added for IP address assignment
 
 using namespace ns3;
 
 int main (int argc, char *argv[])
 {
   CommandLine cmd;
-  int macroCells_N = 9;
-  int microCells_N = 9;
-  int UE_N = 50;
   Ptr<LteHelper> lteHelper = CreateObject<LteHelper> ();
 
-  // macro cells
+  // 9 macro cells
   NodeContainer macroNodes;
-  macroNodes.Create(macroCells_N);
-
+  macroNodes.Create(9);
+  
   Ptr<ListPositionAllocator> positionAlloc = CreateObject<ListPositionAllocator>();
   positionAlloc->Add (Vector(5000, 5000, 0));
   positionAlloc->Add (Vector(0, 5000, 0));
@@ -35,106 +32,73 @@ int main (int argc, char *argv[])
   macroMobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
   macroMobility.SetPositionAllocator(positionAlloc);
   macroMobility.Install (macroNodes);
-
-  // micro cells
+  
+  // 30 micro cells
   NodeContainer microNodes;
-  microNodes.Create(microCells_N);
-
+  microNodes.Create(30);
+  
   Ptr<UniformRandomVariable> x = CreateObject<UniformRandomVariable>();
   x->SetAttribute ("Min", DoubleValue (-5000));
   x->SetAttribute ("Max", DoubleValue (5000));
+
   Ptr<UniformRandomVariable> y = CreateObject<UniformRandomVariable>();
   y->SetAttribute ("Min", DoubleValue (-5000));
   y->SetAttribute ("Max", DoubleValue (5000));
+  
   Ptr<RandomRectanglePositionAllocator> microPositionAlloc = CreateObject<RandomRectanglePositionAllocator>();
   microPositionAlloc->SetX (x);
   microPositionAlloc->SetY (y);
-  
+
   MobilityHelper microMobility;
   microMobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
   microMobility.SetPositionAllocator(microPositionAlloc);
   microMobility.Install (microNodes);
 
-  // UEs
+  // 50 UEs
   NodeContainer ueNodes;
-  ueNodes.Create (UE_N);
-
+  ueNodes.Create (50);
+  
   MobilityHelper ueMobility;
-  ueMobility.SetPositionAllocator ("ns3::RandomDiscPositionAllocator",
-  "X", DoubleValue (0.0),
-  "Y", DoubleValue (0.0),
-  "Rho", StringValue ("ns3::UniformRandomVariable[Min=0.0|Max=5000.0]"));
+  ueMobility.SetPositionAllocator ("ns3::UniformRandomDiscPositionAllocator",
+                                    "X", DoubleValue (0.0),
+                                    "Y", DoubleValue (0.0),
+                                    "Rho", DoubleValue (5000));
   ueMobility.SetMobilityModel ("ns3::RandomWalk2dMobilityModel",
-  "Mode", StringValue ("Time"),
-  "Time", StringValue ("2s"),
-  "Speed", StringValue ("ns3::ConstantRandomVariable[Constant=1000]"),
-  "Bounds", RectangleValue (Rectangle (-5000, 5000, -5000, 5000)));
+                               "Mode", StringValue ("Time"),
+                               "Time", StringValue ("2s"),
+                               "Speed", StringValue ("ns3::ConstantRandomVariable[Constant=20]"),
+                               "Bounds", RectangleValue (Rectangle (-5000, 5000, -5000, 5000)));
   ueMobility.Install (ueNodes);
 
   // Install LTE protocol stack
   NetDeviceContainer enbDevices;
   NetDeviceContainer ueDevices;
   
-  enbDevices = lteHelper->InstallEnbDevice(macroNodes);
-  ueDevices = lteHelper->InstallUeDevice(ueNodes);
+  enbDevices = lteHelper->InstallEnbDevice (macroNodes);
+  ueDevices = lteHelper->InstallUeDevice (ueNodes);
 
-  /*Handover*/
-
-  // Assign IP addresses to UEs
-  InternetStackHelper internet;
-  internet.Install (ueNodes);
-
-  Ipv4AddressHelper ipv4;
-  ipv4.SetBase ("10.1.1.0", "255.255.255.0");
-  Ipv4InterfaceContainer ueIpIface = ipv4.Assign (ueDevices);
-
-  /*Handover END*/
-
-  //Attach the UEs to an eNB. This will configure each UE according to the eNB configuration, and create an RRC connection between them:
-  lteHelper->Attach (ueDevices, enbDevices.Get (0));
-
-  //Activate a data radio bearer between each UE and the eNB it is attached to:
-  enum EpsBearer::Qci q = EpsBearer::GBR_CONV_VOICE;
-  EpsBearer bearer (q);
-  lteHelper->ActivateDataRadioBearer (ueDevices, bearer);
+  // Attach UEs to eNBs
+  lteHelper->Attach (ueDevices); // for UEs
+  lteHelper->Attach (enbDevices.Get(0)); // for eNBs (just first eNB here)
   
-  //Set the stop time
+  // Activate data radio bearers
+  lteHelper->ActivateDataRadioBearer (ueDevices, EpsBearer (EpsBearer::GBR_CONV_VOICE));
+
+  // Stop time
   Simulator::Stop (Seconds (20));
 
-  ///This is needed otherwise the simulation will last forever, because (among others) the start-of-subframe event is scheduled repeatedly, and the ns-3 simulator scheduler will hence never run out of events.
-  lteHelper->EnablePhyTraces ();
-  lteHelper->EnableMacTraces ();
-  lteHelper->EnableRlcTraces ();
+  // Traces
+  lteHelper->EnableTraces ();
+
+  // Animation
+  AnimationInterface anim ("lte-macro-micro.xml");
   
-  // Enable animation
-  AnimationInterface anim("lte-macro-micro.xml");
-  anim.EnablePacketMetadata(true);
+  anim.SetMaxPktsPerTraceFile (1000000);
 
-  // Set color for each node
-  for (int i = 0; i < macroCells_N; ++i) {
-    anim.UpdateNodeColor(macroNodes.Get(i), 0, 0, 0);  // RGB values
-    anim.UpdateNodeSize(macroNodes.Get(i), 180, 180);  // Set node size
-  }
-  for (int i = 0; i < microCells_N; ++i) {
-    anim.UpdateNodeColor(microNodes.Get(i), 255, 0, 0);  // RGB values
-    anim.UpdateNodeSize(microNodes.Get(i), 130,130);  // Set node size
-  }
-  for (int i = 0; i < UE_N; ++i) {
-    anim.UpdateNodeColor(ueNodes.Get(i), 0, 255, 0);  // RGB values
-    anim.UpdateNodeSize(ueNodes.Get(i), 80,80);  // Set node size
-  }
-
-  // Display IP addresses
-  for (uint32_t i = 0; i < ueNodes.GetN (); ++i) {
-    Ptr<Ipv4> ipv4 = ueNodes.Get (i)->GetObject<Ipv4> ();
-    Ipv4Address addr = ipv4->GetAddress (1, 0).GetLocal ();
-    std::cout << "UE " << i + 1 << " IP address: " << addr << std::endl;
-  }
-
-  anim.SetMobilityPollInterval(Seconds(0.10));
-  anim.SetMaxPktsPerTraceFile (100000000000);
-  // anim.EnablePacketMetadata(true);
+  // Run simulation
   Simulator::Run ();
+
   Simulator::Destroy ();
+  
   return 0;
 }
